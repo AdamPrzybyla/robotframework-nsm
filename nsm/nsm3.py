949571668d87b97aeb6b74bf64bb2f55bfa16f11
Adam Przybyla <adam.przybyla@gmail.com>
2022-04-10 21:34:51 +0200

    new version 0.24 with ubuntu 20.04 scripts

--- nsm/nsm3.py
+++ nsm/nsm3.py
@@ -79,4 +79,4 @@ class gennsm3(type):
 		return kw.run(EXECUTION_CONTEXTS.current)
 
 class nsm3(object,metaclass=gennsm3):
-	ROBOT_LIBRARY_VERSION = '0.23'
+	ROBOT_LIBRARY_VERSION = '0.24'
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
   name = 'robotframework-nsm',
   packages = ['nsm'],
-  version = '0.23',
+  version = '0.24',
   license='MIT',
   description = 'Robotframework library for NSM',
   long_description='''Robotframework-NSM
@@ -92,7 +92,7 @@ available:
   author = 'Adam Przybyla',
   author_email = 'adam.przybyla@gmail.com',
   url = 'https://github.com/AdamPrzybyla/robotframework-nsm',
-  download_url = 'https://github.com/AdamPrzybyla/robotframework-nsm/archive/v_23.tar.gz',
+  download_url = 'https://github.com/AdamPrzybyla/robotframework-nsm/archive/v_24.tar.gz',
   keywords = ['robotframework', 'nsm', 'automatisation', 'tests'],
   install_requires=[
           'robotframework',
@@ -101,6 +101,7 @@ available:
     'Development Status :: 3 - Alpha',
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.9',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 2.7',
