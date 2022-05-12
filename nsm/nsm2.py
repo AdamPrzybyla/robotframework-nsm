ffad8f1af380808b3b7c835fa4b19f178a206fa1
Adam Przybyla <adam.przybyla@gmail.com>
2022-05-12 23:51:17 +0200

    ver 0.28

--- nsm/nsm2.py
+++ nsm/nsm2.py
@@ -42,7 +42,7 @@ class genNSM(type):
 				getattr(cls,n).__func__.robot_name=name.decode("utf-8")
 			
 class nsm(object):
-	ROBOT_LIBRARY_VERSION = '0.27'
+	ROBOT_LIBRARY_VERSION = '0.28'
 	__metaclass__ = genNSM
 
 	def call_p1(self,p1):
